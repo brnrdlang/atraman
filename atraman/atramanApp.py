9533c0819dae3af512914d1e0b7f607bcda86bee
collurio229 <bernhard.lang@gmx.de>
2013-10-14 00:32:32 +0200

    Created layout of the CalendarWidget

--- atraman/atramanApp.py
+++ atraman/atramanApp.py
@@ -1,32 +1,61 @@
 from datetime import datetime, date
+from calendar import Calendar
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ListProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+import os
+
+pwd = os.path.dirname(__file__)
+
+class DateButton(Button):
+    def __init__(self, date, callback, **kwargs):
+        super(DateButton, self).__init__(**kwargs)
+        self.date = date
+        self.bind(on_press = callback)
+
+#Builder.load_file(os.path.join(pwd, 'calendar.kv'))
 
 class CalendarWidget(BoxLayout):
     
     month = StringProperty('')
     year = StringProperty('')
+    calendar_layout = ObjectProperty(None)
+    calendar_btns = ListProperty([])
+    calendar = ObjectProperty(Calendar())
 
     def __init__(self, **kwargs):
         super(CalendarWidget, self).__init__(**kwargs)
+
         self.date = date.today()
         self.month = self.date.strftime('%B')
         self.year = str(self.date.year)
 
+    def on_calendar_layout(self, instnce, value):
+        self.create_calendar()
+
+    def create_calendar(self):
+        self.calendar_layout.clear_widgets()
+        def do_day_clicked(instance):
+            pass
+
+        for week in self.calendar.monthdatescalendar(self.date.year, self.date.month):
+            self.calendar_btns.extend([DateButton(date, do_day_clicked,text=str(date.day), name=str(date))
+                             for date in week])
+
+        for btn in self.calendar_btns:
+            self.calendar_layout.add_widget(btn)
+
     def do_month_before(self):
         self.date = date(self.date.year, self.date.month - 1, 1)
-        
 
     def do_month_next(self):
         pass
 
-    def do_day_clicked(self, day):
-        pass
-
+    
 #will be used later when there is more than the calendar
 class MenuScreen(Screen):
     pass
